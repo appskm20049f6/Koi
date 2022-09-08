e00cc09c20465ce584c4498e99e5343ec244a862
Koi tw <appskm20049f6@gmail.com>
2022-09-08 16:34:54 +0800

    09081630

---
+++ src/components/banner.vue
@@ -0,0 +1,20 @@
+
+    <template>
+
+    <div class="banner">
+        <img src="../assets/ExBanner.png">
+    </div>
+
+   </template>
+   
+   <script setup>
+   
+  
+   
+   </script>
+   
+  <style>
+
+  </style>
+   
+   
---
+++ src/components/topbar.vue
@@ -0,0 +1,63 @@
+
+<template>
+
+ <div class="topbar">
+        <div class="menu">
+            <div class="menuicon">
+                <img src="../assets/menuicon.png">
+            </div>
+        </div>
+        <div class="search">
+            <input
+                type="text"
+                placeholder=" 搜尋標題 / NFT......"
+            >
+        </div>
+        <div class="profile">
+            <img src="../assets/profile.png">
+        </div>
+    </div>
+
+</template>
+
+<script setup>
+
+
+const topbar = {
+    
+}
+
+</script>
+
+
+<style scoped>
+    .menuicon{
+        position:absolute;
+
+
+    }
+.topbar {
+    display: flex;
+    height: 51px;
+    justify-content: space-around;
+}
+
+.topbar input {
+    width: 296px;
+    height: 24px;
+    border-radius: 10px;
+    background-color: #292929;
+    border-color: rgb(255, 255, 255);
+    text-indent: 30px;
+    line-height: 51px;
+    box-sizing: border-box;
+}
+
+.menu {
+    display: flex;
+    margin: auto;
+    margin-left: 3%;
+    box-sizing: border-box;
+
+}
+</style>
