6b046d8e8b1d3f8e2efcb9ec7058ff0401284ed3
Koi tw <appskm20049f6@gmail.com>
2022-10-19 14:22:19 +0800

    2022 10 19

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import Topbar from "./components/Topbar.vue";
+</script>
+
+<template>
+  <Topbar />
+</template>
+
+<style leng="scss"></style>
---
+++ src/components/Topbar.vue
@@ -0,0 +1,21 @@
+<script setup></script>
+
+<template>
+  <div class="navbar">
+    <div class="logo"><img src="../img/GFi_960x250.png" alt="" /></div>
+    <ul>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+    </ul>
+    <div class="input"></div>
+    <div class="hint"></div>
+    <div class="setting"></div>
+  </div>
+</template>
+
+<style leng="sass">
+@import "../assets/scss/_RWD.scss";
+</style>
