d06cb5f0d78b8385862fa9a934acad46078bd415
Koi tw <appskm20049f6@gmail.com>
2022-10-19 09:15:58 +0800

    20221019 0915 vite初體驗紀錄

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,9 @@
 <script setup>
 import topbar from './components/topbar.vue'
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
   <topbar></topbar>
-  <HelloWorld></HelloWorld>
 
 </template>
 
