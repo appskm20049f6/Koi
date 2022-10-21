c2be2164a9279fceee3f6a8fd47ca07a180548ac
Koi tw <appskm20049f6@gmail.com>
2022-10-21 16:58:50 +0800

    20221021 1658

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,6 @@ import indexPage1Vue from "./components/index-page1.vue";
 import Topbar from "./components/Topbar.vue";
 </script>
 
-<template>
-  <Topbar />
-  <indexPage1Vue />
-</template>
+<template><Topbar /> <indexPage1Vue /></template>
 
 <style leng="scss"></style>
