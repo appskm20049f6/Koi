0d3121cdc6618ce32cff1e6d0b30f9b423de38a6
Koi tw <appskm20049f6@gmail.com>
2022-10-27 11:33:05 +0800

    20221027

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,9 @@
 <script setup>
-import topbar from './components/topbar.vue'
+import topbar from "./components/topbar.vue";
 </script>
 
 <template>
   <topbar></topbar>
-
 </template>
 
-<style lang="scss">
-
-
-
-</style>
+<style lang="scss"></style>
